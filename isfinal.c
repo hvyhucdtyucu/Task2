5a0842b3541c74ce186f1cf5259d2d02ea3b4ca2
KitsuneZenko <61026708+hvyhucdtyucu@users.noreply.github.com>
2025-01-05 16:46:13 +0200

    Finalized version completed

--- isfinal.c
+++ isfinal.c
@@ -85,40 +85,40 @@ int main() {
     initialize_catalog();
 
     for (int i = 0; i < CLIENT_COUNT; i++) {
-        pipe(parent_to_client[i]);
-        pipe(client_to_parent[i]);
-
-        if ((pids[i] = fork()) == 0) {
-            close(parent_to_client[i][1]);
-            close(client_to_parent[i][0]);
+        pipe(parent_to_client[i]); // create pipe for parent-to-client 
+        pipe(client_to_parent[i]); // create pipe for client-to-parent
+// write = 1 read =0
+        if ((pids[i] = fork()) == 0) { // child process
+            close(parent_to_client[i][1]); //close unused write in child
+            close(client_to_parent[i][0]); // unused read in child
 
             for (int j = 0; j < ORDERS_PER_CLIENT; j++) {
                 srand(time(NULL) ^ getpid());
                 int product_id = rand() % PRODUCT_COUNT;
-                write(client_to_parent[i][1], &product_id, sizeof(int));
+                write(client_to_parent[i][1], &product_id, sizeof(int)); //sends item id to parent
 
                 char response[100];
-                read(parent_to_client[i][0], response, sizeof(response));
+                read(parent_to_client[i][0], response, sizeof(response)); // reads response from parent
                 printf("Customer %d: %s\n", i + 1, response);
 
                 sleep(1);
             }
-
+             //close remaining pipes in child
             close(parent_to_client[i][0]);
             close(client_to_parent[i][1]);
             exit(0);
-        } else {
-            close(parent_to_client[i][0]);
-            close(client_to_parent[i][1]);
+        } else { //parent
+            close(parent_to_client[i][0]); //close unsuded read in parent
+            close(client_to_parent[i][1]); // close unused write in parent
         }
     }
 
     for (int i = 0; i < CLIENT_COUNT * ORDERS_PER_CLIENT; i++) {
         int product_id;
-        read(client_to_parent[i % CLIENT_COUNT][0], &product_id, sizeof(int));
+        read(client_to_parent[i % CLIENT_COUNT][0], &product_id, sizeof(int)); //read product id from client
         process_order(product_id, "Some Client", parent_to_client[i % CLIENT_COUNT]);
     }
-
+//wait for all child processes to finish
     for (int i = 0; i < CLIENT_COUNT; i++) {
         wait(NULL);
     }
